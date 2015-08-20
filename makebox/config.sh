b50d26406856cd432797195e4bd08191e804b15b
Richard Price <rich@gandalf.ws>
2015-08-20 14:56:51 -0500

    Added FourthAge-JCR01. Modified 1848-PAZ01. Modified 1861-PAZ02. Modified 1880-REP02. And deleted old [pre library] tile images.

--- makebox/config.sh
+++ makebox/config.sh
@@ -1,5 +1,5 @@
 # This file must be updated with the proper parameter before
-# BoxMake is run to create a new BOARD18 game box.
+# gameBoxMake is run to create a new BOARD18 game box.
 
 # Set boxesRoot to the boxes directory in the GAMEBOX directory structure.
 boxesRoot=~/NetBeansProjects/GAMEBOX/boxes/
